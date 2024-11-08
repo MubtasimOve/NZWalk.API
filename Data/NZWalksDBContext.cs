cb79f1b0103490bd3c0521353f90841e246ba89b
Mubtasim Ihtisham Ove <mubtasim26@gmail.com>
2024-11-08 23:37:59 +0600

    Before Migration

---
+++ Data/NZWalksDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Model.Domain_Model;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDBContext : DbContext 
+    {
+        public NZWalksDBContext(DbContextOptions dbContextOption) : base(dbContextOption) 
+        {
+                
+        }
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+
+        public DbSet<Walk> Walks { get; set; }
+    }
+}
